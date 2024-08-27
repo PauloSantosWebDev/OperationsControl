23605f84080c2006d90a3da19715e4bf6f1a552f
PauloSantosWebDev <paulosantos@outlook.com.au>
2024-08-27 18:41:02 +0930

    Creating nested inheritance

--- public/javascripts/daysheet.js
+++ public/javascripts/daysheet.js
@@ -53,63 +53,32 @@ function createFields() {
  
 
 function AssetPersonnelFields(qAssets = 0, qPersonnel = 0) {
-
     let accumulator = "";
-
     for(let i = 0; i < qAssets; i++){
-
         accumulator = accumulator +
-
         `<div class="col-md-12">
-
             <label for="inputAssets${i + 1}" class="form-label">Asset - ${i + 1}</label>
-
             <select id="inputAssets${i + 1}" class="form-select">
-
                 <option>working</option>
-
             </select>
-
         </div>`    
-
     }  
-
     document.getElementById('formAsset').innerHTML = accumulator;
-
- 
 
     accumulator = "";
 
- 
-
     for(let i = 0; i < qPersonnel; i++) {
-
         accumulator = accumulator +
-
         `<div class="col-md-12">
-
             <label for="inputPersonnel${i + 1}" class="form-label">Personnel - ${i + 1}</label>
-
             <select id="inputPersonnel${i + 1}" class="form-select">
-
                 <option>working</option>
-
             </select>
-
         </div>`
-
     }
-
     document.getElementById('formPersonnel').innerHTML = accumulator
-
- 
-
 }
 
- 
-
 //Event listener
-
 document.getElementById('inputNumberAssets').addEventListener('change', createFields);
-
 document.getElementById('inputNumberEmployees').addEventListener('change', createFields);
